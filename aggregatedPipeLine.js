97b4041541625405a1827dc76852dc64924c717e
Tanvir Rudro <rudro987@gmail.com>
2024-05-15 15:53:22 +0600

    higher level overview of aggregated pipeline

--- aggregatedPipeLine.js
+++ aggregatedPipeLine.js
@@ -19,5 +19,71 @@ db.test.aggregate([
 db.test.aggregate([
     { $group: { _id: "$isActive", count: { $sum: 1 }, fullDoc: { $push: "$$ROOT" } } },
     { $project: { "fullDoc.name": 1, "fullDoc.phone": 1, "fullDoc.email": 1 } }
+]);
+
+
+
+db.test.aggregate([
+    {
+        $group: {
+            _id: null,
+            totalAge: { $sum: "$age" },
+            maxAge: {$max: "$age"},
+            minAge: {$min: "$age"},
+            avgAge: {$avg: "$age"}
+        }
+
+    },
+    {
+        $project: {
+            totalAge: 1,
+            maxAge: 1,
+            minAge: 1,
+            averageAge: "$avgAge",
+            rangeBetweenMaxAndMin: {$subtract: ["$maxAge","$minAge"]}
+        }
+    }
+
 ])
+
+
+
+db.test.aggregate([
+    {
+        $unwind: "$interests"
+    },
+    {
+        $group: {_id: "$age", interestsPerAge: {$push: "$interests"}}
+    }
+]);
+
+
+
+db.test.aggregate([
+    {
+        $bucket: {
+            groupBy: "$age",
+            boundaries: [20, 40, 60, 80],
+            default: "People aged above 80",
+            output: {
+                count: { $sum: 1 },
+                name: { $push: "$$ROOT" }
+            }
+        }
+    },
+    {
+        $sort: { count: -1 }
+    },
+    {
+        $limit: 3
+    },
+    {
+        $project: { count: 1 }
+    }
+]);
+
+
+
+
+
 
